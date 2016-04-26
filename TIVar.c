dc2bb10420235d38f7b3b92e378dee1971d4f8ae
jl4ge <jl4ge@virginia.edu>
2016-04-25 20:52:39 -0400

    changes

---
+++ TIVar.c
@@ -0,0 +1,10 @@
+/*
+ * TIVar.c
+ *
+ *  Created on: Apr 25, 2016
+ *      Author: Jonathan Lowe
+ */
+
+
+
+
---
+++ TIVar.h
@@ -0,0 +1,15 @@
+/*
+ * TIVar.h
+ *
+ *  Created on: Apr 25, 2016
+ *      Author: Jonathan Lowe
+ */
+
+#ifndef TIVAR_H_
+#define TIVAR_H_
+
+
+
+
+
+#endif /* TIVAR_H_ */
